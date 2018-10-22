741fd6664908b85ded83a20fd626726c0d724db4
sallachim <44349427+sallachim@users.noreply.github.com>
2018-10-22 23:40:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include "Login.h"
+#include "Date.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    Date x;
+    x.day = 22;
+    x.month = 10;
+    x.year = 2018;
+
+    Login notKnown;
+    notKnown.entryCheck();
+    system ("pause");
     return 0;
 }
